4b2a33982bba401929e0e7453cfa692aa82c701d
Alex Hyams <111457482+tribayAlex@users.noreply.github.com>
2023-05-29 13:33:15 -0400

    Modified code for shopvox, added element properties for customizability

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -21,8 +25,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const tmp = __importStar(require("tmp"));
-const js2xmlparser = __importStar(require("js2xmlparser"));
-const switchecommerce = __importStar(require("switchecommerce"));
 //Is always required even if the function is not used (timer fired)
 async function jobArrived(s, flowElement, job) {
 }
@@ -33,7 +35,7 @@ async function jobArrived(s, flowElement, job) {
  */
 async function flowStartTriggered(s, flowElement) {
     let api_token = await flowElement.getPropertyStringValue("api_token");
-    let webhookPath = "/eCommerce";
+    let webhookPath = await flowElement.getPropertyStringValue("uri");
     try {
         await s.httpRequestSubscribe(HttpRequest.Method.POST, webhookPath, [api_token]);
     }
@@ -52,7 +54,7 @@ async function flowStartTriggered(s, flowElement) {
 async function httpRequestTriggeredSync(request, args, response, s) {
     let eCommerceData = request.getBodyAsString();
     let eCommerceParse = JSON.parse(eCommerceData);
-    let jobID = eCommerceParse.orderId.toString();
+    let jobID = eCommerceParse.id.toString();
     let processedIDS = {};
     let idsFromGlobalData = await s.getGlobalData(Scope.FlowElement, "uuids");
     if (idsFromGlobalData !== "") {
@@ -69,7 +71,7 @@ async function httpRequestTriggeredSync(request, args, response, s) {
         response.setHeader('Content-Type', 'application/json');
         response.setHeader('api_token', args[0]);
         response.setBody(Buffer.from(JSON.stringify({ "result": "success", "orderID": jobID, "api_token": args[0] })));
-        processedIDS[jobID] = { arrival: new Date().getTime(), product: eCommerceParse.product.productName };
+        processedIDS[jobID] = { arrival: new Date().getTime(), name: eCommerceParse.name };
         await s.setGlobalData(Scope.FlowElement, 'uuids', JSON.stringify(processedIDS));
     }
 }
@@ -82,26 +84,17 @@ async function httpRequestTriggeredSync(request, args, response, s) {
  */
 async function httpRequestTriggeredAsync(request, args, s, flowElement) {
     //Parse JSON from Body
-    let eCommerceData = request.getBodyAsString();
-    var eCommerceParse = JSON.parse(eCommerceData);
-    let downloadPath = eCommerceParse.filename;
-    let downloadFileName = eCommerceParse.filename.split("/").pop();
-    let jobID = eCommerceParse.orderId;
+    await flowElement.log(LogLevel.Debug, 'Webhook triggered!');
+    let data = request.getBodyAsString();
+    var dataParsed = JSON.parse(data);
     //Define Dataset
-    let xmlString = js2xmlparser.parse("eCommerce", eCommerceParse);
-    let tmpDatasetFile = tmp.fileSync({ postfix: ".xml" }).name;
-    let datasetName = "eCommerce";
-    fs.writeFileSync(tmpDatasetFile, xmlString);
-    //Download pdf file
-    let tmpJobFile = tmp.fileSync({ prefix: "download_" }).name;
-    await flowElement.log(LogLevel.Info, "Download of job " + jobID + " in progress");
-    let result = await switchecommerce.eCommerceDownloadFile(downloadPath, downloadFileName, jobID);
-    await flowElement.log(LogLevel.Info, "Download of job " + jobID + " finished");
+    let tmpDatasetFile = tmp.fileSync({ postfix: ".json" }).name;
+    let datasetName = await flowElement.getPropertyStringValue('datasetName');
+    fs.writeFileSync(tmpDatasetFile, data);
     //Create job containing the production file and define dataset
-    let job = await flowElement.createJob(result.filePath);
-    await job.createDataset(datasetName, tmpDatasetFile, DatasetModel.XML);
-    await job.sendToSingle(result.fileName);
-    fs.unlinkSync(tmpJobFile);
+    let job = await flowElement.createJob(tmpDatasetFile);
+    await job.createDataset(datasetName, tmpDatasetFile, DatasetModel.JSON);
+    await job.sendToSingle(dataParsed.id + '.json');
     fs.unlinkSync(tmpDatasetFile);
 }
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 import * as fs from "fs";
 import * as tmp from "tmp";
-import * as js2xmlparser from "js2xmlparser";
-import * as switchecommerce from "switchecommerce"
 
 //Is always required even if the function is not used (timer fired)
 async function jobArrived(s: Switch, flowElement: FlowElement, job: Job) {
@@ -14,7 +12,7 @@ async function jobArrived(s: Switch, flowElement: FlowElement, job: Job) {
  */
  async function flowStartTriggered(s: Switch, flowElement: FlowElement) {
     let api_token = await flowElement.getPropertyStringValue("api_token") as string;
-    let webhookPath = "/eCommerce";
+    let webhookPath = await flowElement.getPropertyStringValue("uri") as string;
     try {
       await s.httpRequestSubscribe(HttpRequest.Method.POST, webhookPath, [api_token]);
     } catch (error) {
@@ -33,7 +31,7 @@ async function jobArrived(s: Switch, flowElement: FlowElement, job: Job) {
 async function httpRequestTriggeredSync(request: HttpRequest, args: any[], response: HttpResponse, s: Switch) {
     let eCommerceData = request.getBodyAsString();
     let eCommerceParse = JSON.parse(eCommerceData)
-    let jobID = eCommerceParse.orderId.toString();
+    let jobID = eCommerceParse.id.toString();
     let processedIDS: Record<string, any> = {}
     let idsFromGlobalData = await s.getGlobalData(Scope.FlowElement, "uuids")
     if (idsFromGlobalData !== "") {
@@ -50,7 +48,7 @@ async function httpRequestTriggeredSync(request: HttpRequest, args: any[], respo
       response.setHeader('Content-Type', 'application/json');
       response.setHeader('api_token', args[0]);
       response.setBody(Buffer.from(JSON.stringify({"result":"success","orderID":jobID,"api_token": args[0]})));
-      processedIDS[jobID] = { arrival: new Date().getTime(), product: eCommerceParse.product.productName }
+      processedIDS[jobID] = { arrival: new Date().getTime(), name: eCommerceParse.name }
       await s.setGlobalData(Scope.FlowElement, 'uuids', JSON.stringify(processedIDS));
     }
   }
@@ -64,28 +62,18 @@ async function httpRequestTriggeredSync(request: HttpRequest, args: any[], respo
  */
  async function httpRequestTriggeredAsync(request: HttpRequest, args: any[], s: Switch, flowElement: FlowElement) {
   //Parse JSON from Body
-  let eCommerceData = request.getBodyAsString();
-  var eCommerceParse = JSON.parse(eCommerceData)
-  let downloadPath = eCommerceParse.filename
-  let downloadFileName = eCommerceParse.filename.split("/").pop()
-  let jobID = eCommerceParse.orderId
+  await flowElement.log(LogLevel.Debug,'Webhook triggered!');
+  let data = request.getBodyAsString();
+  var dataParsed = JSON.parse(data);
 
   //Define Dataset
-  let xmlString = js2xmlparser.parse("eCommerce", eCommerceParse)
-  let tmpDatasetFile = tmp.fileSync({ postfix: ".xml" }).name;
-  let datasetName = "eCommerce";
-  fs.writeFileSync(tmpDatasetFile, xmlString);
-  
-  //Download pdf file
-  let tmpJobFile = tmp.fileSync({ prefix: "download_" }).name;
-  await flowElement.log(LogLevel.Info, "Download of job " + jobID + " in progress")
-  let result = await switchecommerce.eCommerceDownloadFile(downloadPath, downloadFileName, jobID)
-  await flowElement.log(LogLevel.Info, "Download of job " + jobID +  " finished")
+  let tmpDatasetFile = tmp.fileSync({ postfix: ".json" }).name;
+  let datasetName = await flowElement.getPropertyStringValue('datasetName') as string;
+  fs.writeFileSync(tmpDatasetFile, data);
 
   //Create job containing the production file and define dataset
-  let job = await flowElement.createJob(result.filePath);
-  await job.createDataset(datasetName, tmpDatasetFile, DatasetModel.XML);
-  await job.sendToSingle(result.fileName);
-  fs.unlinkSync(tmpJobFile);
+  let job = await flowElement.createJob(tmpDatasetFile);
+  await job.createDataset(datasetName, tmpDatasetFile, DatasetModel.JSON);
+  await job.sendToSingle(dataParsed.id + '.json');
   fs.unlinkSync(tmpDatasetFile);
 }
